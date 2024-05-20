a68f16e247128916fc78f748083069f2207b5ccf
Nicholas Lima <95785812+Nicholas-Liman@users.noreply.github.com>
2024-05-20 18:53:58 -0300

    Update page.tsx

--- src/app/Personagens/page.tsx
+++ src/app/Personagens/page.tsx
@@ -22,10 +22,10 @@ const Personagens = () => {
                 <div className="conjunto-propositos-personagens">
                     <a className="organizacao-personagens"><img src={ Einstein.src } className="personagem-personagens"/></a>
                     <div className="texto-personagens">
-                            <h1 className="titulo-personagens">Einstei</h1>
-                            <h2 className="subtitulo-personagens"><b>O gêni de todos</b></h2>
-                            <p className="texto-esquerda">Einstin é o cientista de dados da Salsforce. Nasceu em 2016 e fo criado com bse em um dos maires gênios da hitória mundial.
-                                 Além de suer rápido, ele é o cérebro por trás da inteligência artificial da empresa. 
+                            <h1 className="titulo-personagens">Einstein</h1>
+                            <h2 className="subtitulo-personagens"><b>O maior gênio de todos</b></h2>
+                            <p className="texto-esquerda">Einstein é o cientista de dados da Salesforce. Nasceu em 2016 e foi criado com base em um dos maiores gênios da história mundial.
+                                 Além de super rápido, ele é o cérebro por trás da inteligência artificial da empresa. 
                                  Tem um produto que leva seu nome: 
                                  uma plataforma unificada que identifica oportunidades de vendas, prevê resultados e 
                                  faz recomendações inteligentes. </p>
